3c64ad3ebe4e2801f34bb833b1a904b056ccf35d
Estefania Castro Vizoso <estefania.castro.vizoso@gmail.com>
2021-03-20 20:25:44 +0100

    Delete springboot-books-api/reactive-api/target directory

--- springboot-books-api/reactive-api/target/generated-sources/spring-aot/src/main/java/org/springframework/data/r2dbc/dialect/_FactoryProvider.java
+++
@@ -1,7 +0,0 @@
-package org.springframework.data.r2dbc.dialect;
-
-public abstract class _FactoryProvider {
-  public static DialectResolver.BuiltInDialectProvider dialectResolverBuiltInDialectProvider() {
-    return new DialectResolver.BuiltInDialectProvider();
-  }
-}
--- springboot-books-api/reactive-api/target/generated-sources/spring-aot/src/main/java/org/springframework/r2dbc/core/binding/_FactoryProvider.java
+++
@@ -1,8 +0,0 @@
-package org.springframework.r2dbc.core.binding;
-
-public abstract class _FactoryProvider {
-  public static BindMarkersFactoryResolver.BuiltInBindMarkersFactoryProvider bindMarkersFactoryResolverBuiltInBindMarkersFactoryProvider(
-      ) {
-    return new BindMarkersFactoryResolver.BuiltInBindMarkersFactoryProvider();
-  }
-}
