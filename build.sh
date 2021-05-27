49d84928f734177eb0fef498e5004f8de37a9471
Estefania Castro Vizoso <estefania.castro.vizoso@gmail.com>
2021-05-27 22:00:04 +0200

    all of them working, pending to use exactly the same query

---
+++ build.sh
@@ -0,0 +1,12 @@
+
+mvn package -Pnative -Dquarkus.native.container-build=true -pl quarkus-imperative-jpa-api
+docker build -f src/main/docker/Dockerfile.native -t quarkus/quarkus-imperative-jpa-api .
+
+mvn package -Pnative -Dquarkus.native.container-build=true -pl quarkus-imperative-jdbc-api
+docker build -f src/main/docker/Dockerfile.native -t quarkus/quarkus-imperative-jdbc-api .
+
+mvn package -Pnative -Dquarkus.native.container-build=true -pl quarkus-reactive-api
+docker build -f src/main/docker/Dockerfile.native -t quarkus/quarkus-reactive-api .
+
+
+docker-compose up
