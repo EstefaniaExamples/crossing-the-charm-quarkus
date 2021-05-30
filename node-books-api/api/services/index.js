62166836226f8aa2411bc3cc7472d054ac97c676
Estefania Castro Vizoso <estefania.castro.vizoso@gmail.com>
2021-05-30 22:50:24 +0200

    node project same query

--- node-books-api/api/services/index.js
+++ node-books-api/api/services/index.js
@@ -5,7 +5,7 @@ class Book {
     let results = await db
     .query(`select b.*,
           (
-              select json_agg(json_build_object('authorId', a.id, 'authorName', concat_ws(' ', a.name, a.surname)))
+              select json_agg(json_build_object('id', a.id, 'name', a.name, 'surname', a.surname))
               from authors a
               join books_authors ba on a.id=ba.author_id
               where b.id=ba.book_id
@@ -18,7 +18,7 @@ class Book {
   async getBookById(bookId) {
     let results = await db
     .query(`SELECT b.id, b.title, b.description,
-          ( SELECT json_agg(json_build_object('authorId', a.id, 'fullName', concat_ws(' ', a.name, a.surname)))
+          ( select json_agg(json_build_object('id', a.id, 'name', a.name, 'surname', a.surname))
                         FROM authors a JOIN books_authors ba ON a.id=ba.author_id
                          WHERE b.id=ba.book_id ) as authors
           FROM books b  WHERE b.id=$1`, [parseInt(bookId)])
