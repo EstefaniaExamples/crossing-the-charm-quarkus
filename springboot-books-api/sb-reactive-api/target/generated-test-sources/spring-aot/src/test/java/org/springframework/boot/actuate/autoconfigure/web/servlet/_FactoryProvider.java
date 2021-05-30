d30660f7bbdc9d0ed218886fe6bbe68c05fc9185
Estefania Castro Vizoso <estefania.castro.vizoso@gmail.com>
2021-05-30 23:14:23 +0200

    udpate git ignore and delete target folders

--- springboot-books-api/sb-reactive-api/target/generated-test-sources/spring-aot/src/test/java/org/springframework/boot/actuate/autoconfigure/web/servlet/_FactoryProvider.java
+++
@@ -1,8 +0,0 @@
-package org.springframework.boot.actuate.autoconfigure.web.servlet;
-
-public abstract class _FactoryProvider {
-  public static ServletManagementChildContextConfiguration servletManagementChildContextConfiguration(
-      ) {
-    return new ServletManagementChildContextConfiguration();
-  }
-}
