442fd17ef389fc83e6c9b38a7e8ed00ba9062028
FanyCastro <estefania.castro.vizoso@gmail.com>
2021-06-01 11:54:37 +0200

    add custom jdbctemplate in sb jdbc api

--- springboot-books-api/sb-imperative-jdbc-api/src/main/java/org/springboot/training/Application.java
+++ springboot-books-api/sb-imperative-jdbc-api/src/main/java/org/springboot/training/Application.java
@@ -3,6 +3,10 @@ package org.springboot.training;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class Application {
@@ -11,5 +15,10 @@ public class Application {
 
 	public static void main(String[] args) {
 		applicationContext = SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public JdbcTemplate jdbcTemplate(final DataSource dataSource) {
+		return new JdbcTemplate(dataSource);
 	}
 }
