525eeff199cf095764ab7eda034ad51ad0d414ae
FanyCastro <estefania.castro.vizoso@gmail.com>
2021-06-02 09:29:16 +0200

    docker per spring proyect

--- build.sh
+++ build.sh
@@ -1,15 +1,16 @@
 
 mvn package -Pnative -Dquarkus.native.container-build=true -pl quarkus-imperative-jpa-api
 docker build -f src/main/docker/Dockerfile.native -t quarkus/quarkus-imperative-jpa-api .
-
 mvn package -Pnative -Dquarkus.native.container-build=true -pl quarkus-imperative-jdbc-api
 docker build -f src/main/docker/Dockerfile.native -t quarkus/quarkus-imperative-jdbc-api .
-
 mvn package -Pnative -Dquarkus.native.container-build=true -pl quarkus-reactive-api
 docker build -f src/main/docker/Dockerfile.native -t quarkus/quarkus-reactive-api .
 
+mvn clean spring-boot:build-image -pl sb-imperative-jdbc-api
+mvn clean spring-boot:build-image -pl sb-imperative-jpa-api
+mvn clean spring-boot:build-image -pl sb-reactive-api
 
-mvn clean -Pnative-image package
-mvn clean spring-boot:build-image
+cd node-books-api
+docker build -t node/node-books-api .
 
 docker-compose up
