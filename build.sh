f862b94e5c7b5c3ae9c78bdddd0cc907783efa04
FanyCastro <estefania.castro.vizoso@gmail.com>
2021-06-10 15:11:07 +0200

    complete docker compose and build.sh

--- build.sh
+++ build.sh
@@ -12,39 +12,46 @@ mvn clean package -pl sb-imperative-jpa-api
 
 cd ./sb-reactive-api/ || return
 docker build -f Dockerfile -t library/sb-reactive-api-jvm:1.0-SNAPSHOT .
-docker-compose up &
 echo "***** SpringBoot reactive docker native reactive container created"
 
 cd ../sb-imperative-jdbc-api/ || return
 docker build -f Dockerfile -t library/sb-imperative-jdbc-api-jvm:1.0-SNAPSHOT .
-docker-compose up &
 echo "***** SpringBoot reactive docker native imperative jdbc container created"
 
 cd ../sb-imperative-jpa-api/ || return
 docker build -f Dockerfile -t library/sb-imperative-jpa-api-jvm:1.0-SNAPSHOT .
-docker-compose up &
 echo "***** SpringBoot reactive docker native imperative jpa container created"
 
 # quarkus project - native image
 cd ../../quarkus-books-api || return
 
-#mvn package -Pnative -Dquarkus.native.container-build=true -pl quarkus-imperative-jpa-api
-#docker build -f src/main/docker/Dockerfile.native -t quarkus/quarkus-imperative-jpa-api .
-#mvn package -Pnative -Dquarkus.native.container-build=true -pl quarkus-imperative-jdbc-api
-#docker build -f src/main/docker/Dockerfile.native -t quarkus/quarkus-imperative-jdbc-api .
-#mvn package -Pnative -Dquarkus.native.container-build=true -pl quarkus-reactive-api
-#docker build -f src/main/docker/Dockerfile.native -t quarkus/quarkus-reactive-api .
+cd ./quarkus-imperative-jpa-api || return
+mvn clean package -Pnative -Dquarkus.native.container-build=true
+docker build -f src/main/docker/Dockerfile.native -t quarkus/quarkus-imperative-jpa-api .
+
+cd ../quarkus-imperative-jdbc-api || return
+mvn clean package -Pnative -Dquarkus.native.container-build=true
+docker build -f src/main/docker/Dockerfile.native -t quarkus/quarkus-imperative-jdbc-api .
+
+cd ../quarkus-reactive-api || return
+mvn clean package -Pnative -Dquarkus.native.container-build=true
+docker build -f src/main/docker/Dockerfile.native -t quarkus/quarkus-reactive-api .
 
 # quarkus project - jvm image
 
-cd ./quarkus-imperative-jdbc-api ||return
+cd ../quarkus-imperative-jdbc-api || return
 mvn clean package
 docker build -f src/main/docker/Dockerfile.jvm -t quarkus/quarkus-imperative-jdbc-api-jvm .
 
-cd ../quarkus-imperative-jpa-api ||return
+cd ../quarkus-imperative-jpa-api || return
 mvn clean package
 docker build -f src/main/docker/Dockerfile.jvm -t quarkus/quarkus-imperative-jpa-api-jvm .
 
-cd ../quarkus-reactive-api ||return
+cd ../quarkus-reactive-api || return
 mvn clean package
 docker build -f src/main/docker/Dockerfile.jvm -t quarkus/quarkus-reactive-api-jvm .
+
+
+cd ../..
+docker-compose down -v
+docker-compose up
