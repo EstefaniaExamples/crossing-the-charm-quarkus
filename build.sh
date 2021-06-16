fe063b710f8b5ab1d418940108c55e466321ca6f
FanyCastro <estefania.castro.vizoso@gmail.com>
2021-06-16 13:15:46 +0200

    fix errors in sh files

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+if [[ -z $1 && -z $2 ]]; then
+  echo "No IP address passed"
+  exit 1
+fi
+
+export IP_ADDRESS=$1
+
+echo "The IP address is: $1"
+
 docker-compose down -v
 docker-compose up
--- compile.sh
+++ compile.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-if [[ -z $1 && -z $2 ]]; then
-  echo "No IP address passed"
-  exit 1
-fi
-
-export IP_ADDRESS=$1
-echo "The IP address is: $1"
-
 # SpringBoot project
 cd springboot-books-api/ || return
 
